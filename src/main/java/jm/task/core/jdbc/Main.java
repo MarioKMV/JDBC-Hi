f991f4051ecf5956f95d7a2f5fd7e007a4f7f4f9
Mario <kraysveta@hotmail.com>
2024-11-18 18:56:12 +0300

    1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,33 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
+//        Class<Driver> driverClass = Driver.class;
+        UserService userService = new UserServiceImpl ();
+//        String sql = """
+// CREATE TABLE user;
+//""";
+        userService.createUsersTable();
+        userService.saveUser("Ivan", "Ivanov", (byte) 5);
+//        System.out.println("User c имненем '" + userService.getAllUsers().get(0).getName() + "' добавлен в базу данных");
+        userService.saveUser("Petr", "Petrov", (byte) 22);
+        userService.saveUser("Svetlana", "Svetikova", (byte) 12);
+        userService.saveUser("Olga", "Avdeeva", (byte) 25);
+        userService.getAllUsers ();
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,35 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    private static final String PASSWORD = "postgres";
+    private static final String USERNAME = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/mario";
+
+    static {
+        loadDriver();
+    }
+private static  void loadDriver(){
+    try {
+        Class.forName ( "org.postgresql.Driver" );
+    } catch (ClassNotFoundException e) {
+        throw new RuntimeException ( e );
+    }
+}
+
+private Util(){
+
+}
+    public static Connection open() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
