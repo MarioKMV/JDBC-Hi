5bdf7e69d113f202e5c6100670330c13607ddd27
Mario <kraysveta@hotmail.com>
2024-11-19 19:09:48 +0300

    non Hiber

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -19,101 +19,36 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    private final SessionFactory sessionFactory = Util.createSessionFactory();
-    public UserDaoHibernateImpl() {
+        public UserDaoHibernateImpl() {
 
     }
 
     @Override
     public void createUsersTable() {
-        try  {
-            Session session = sessionFactory.openSession();
-            session.beginTransaction();
-            session.createNativeQuery(
-                    "create table if not exists users(" +
-                            "id serial primary key, " +
-                            "name varchar(50) not null, " +
-                            "last_name varchar(50) not null, " +
-                            "age smallint not null);").executeUpdate();
-            session.getTransaction().commit();
-            session.close();
-            System.out.println("Table created successfully!");
-        } catch (HibernateException e) {
-            System.out.println(e.getMessage());
-        }
+
     }
 
     @Override
     public void dropUsersTable() {
-        try {
-            Session session = sessionFactory.openSession();
-            session.beginTransaction();
-            session.createNativeQuery("drop table if exists users;").executeUpdate();
-            session.getTransaction().commit();
-            session.close();
-        } catch (HibernateException e) {
-            System.out.println(e.getMessage());
-        }
+
     }
 
     @Override
-    public void saveUser(String name, String lastName, byte age) throws SQLException {
-        try {
-            Session session = sessionFactory.openSession();
-            session.beginTransaction();
-            User user1 = new User();
-            user1.setName(name);
-            user1.setLastName(lastName);
-            user1.setAge(age);
-            session.save(user1);
-            session.getTransaction().commit();
-            session.close();
-        } catch (HibernateException e) {
-            System.out.println(e.getMessage());
-        }
+    public void saveUser(String name, String lastName, byte age) {
+
     }
 
     @Override
     public void removeUserById(long id) throws SQLException {
-        try {
-            Session session = sessionFactory.openSession();
-            session.beginTransaction();
-            User user = session.get(User.class, id);
-            session.remove(user);
-            session.getTransaction().commit();
-            session.close();
-        } catch (HibernateException e) {
-            System.out.println(e.getMessage());
-        }
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        try {
-            Session session = sessionFactory.openSession();
-            session.beginTransaction();
-            List<User> users = session.createQuery("select u from User u", User.class ).list();
-            session.getTransaction().commit();
-            session.close();
-            return users;
-        } catch (HibernateException e) {
-            System.out.println(e.getMessage());
-        }
         return null;
     }
 
     @Override
     public void cleanUsersTable() {
-        try {
-            Session session = sessionFactory.openSession();
-            session.beginTransaction();
-            session.createNativeQuery("truncate table users;").executeUpdate();
-            session.getTransaction().commit();
-            session.close();
-        } catch (HibernateException e) {
-            System.out.println(e.getMessage());
-        }
     }
-
 }
